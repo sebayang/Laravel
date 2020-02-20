c373b6a0fe62252135c233dcce798d2567a1cc42
sebayang <ibrahim.sebayang@gmail.com>
2020-02-20 07:07:05 +0700

    Alredy Using elequent

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+    @extends('layout/main')
+    @section('title', 'About')
+
+    @section('container')
+    <div class="container">
+        <div class="row">
+            <div class="cil-10">
+                <h1 class="mt-2">ini about!</h1>
+            </div>
+        </div>
+    </div>
+    @endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+    @extends('layout/main')
+    @section('title', 'Web Laravel')
+
+    @section('container')
+    <div class="container">
+        <div class="row">
+            <div class="cil-10">
+                <h1 class="mt-2">ini home!</h1>
+            </div>
+        </div>
+    </div>
+    @endsection
